97f06ae57d967ec33ae58104a2f0266ea912b4b6
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-02-20 09:18:18 +0300

    My name and surname are correctly revealed

--- index.js
+++ index.js
@@ -1,5 +1,87 @@
-let myFirstname = "Jyldyz Baryktabasova"
+//My name is Jyldyz  Baryktabasova
+  
+//function nameIs(name) {
+    //console.log("my name is" + name);
+  //}
 
-console.log("My name is" + myFirstname)
+  //function surnameIs(name) {
+    //console.log("my surname is" + name);
+    
+  //}
+  //nameIs("Jyldyz")
+  //surnameIs("Baryktabasova");
+
+  //function getConeVolume(h, r) {
+    //let V = 0
+    //let pi = 3.14
+    //console.log( V = (h * pi * (r  2) / 3));
+  //}
+  //getConeVolume(10., 5);
 
 
+2//functionName(height, radius)
+//function getConeVolume(h, r) {
+  //let V = 0
+  //let pi = 3.14
+  //console.log( V = (h * pi * (r = 2) / 3));
+//}
+//getConeVolume(10., 5);
+
+3// Длина строки равна ...
+
+//function strLength(text) {
+  //console.log(text.length)
+//}
+//strLength("This is another")
+
+4// functionName(num) // Число - четное || Число - нечетное
+//function isEven(num){
+  //if(num % 2 === 0){
+    //console.log("even - " + num)
+  //}
+  //else if(num % 2 === 1){
+    //console.log("odd - " + num)
+  //}
+  //else{
+    //console.log("error - " + num)
+  //}
+//}
+//isEven(4)
+//isEven(7)
+//isEven("seven")
+
+// positive(num) // проверка на положительное число (true || false)
+//function isPositive(num) {
+  //return num >= 0;
+//}
+
+// вернуть положительное число
+//function toPositive(num) {
+  //if (num >= 0) {
+    //return num;
+  //}  else {
+    //return num * -1;
+  //}
+//}
+
+//5 квадрат числа
+//function square(num){
+  //return num = 2
+//}
+//console.log(square(5))
+
+// 6// functionName(num) // Корень этого числа равен
+//function root(a) {
+  //return Math.sqrt(a)
+//}
+//console.log(root(25))
+
+
+// // 7  functionName(a, b, c, d, e) // Периметр - ...
+function triangle(a, b, c, d, e) {
+  return a + b + c + d + e;
+}
+console.log(triangle(5,3,6,9,5))
+
+
+  
